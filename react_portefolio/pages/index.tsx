da504310dc5b1c2365a6954bcd2cde3af5930ea5
eithan <eithan.nakache@epita.fr>
2022-12-13 19:58:21 +0100

    8 version

--- react_portefolio/pages/index.tsx
+++ react_portefolio/pages/index.tsx
@@ -85,7 +85,7 @@ export default function Home() {
             </a>
           </div>
           <div className="relative mx-auto bg-gradient-to-b from-blue-400 rounded-full w-80 h-80 mt-20 overflow-hidden">
-            <Image src={eithan} layout="fill" objectFit="cover" />
+            <Image  alt="Picture of the author" src={eithan} layout="fill" objectFit="cover" />
           </div>
         </section>
 
