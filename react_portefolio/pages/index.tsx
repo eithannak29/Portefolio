e0d068f4fbe3d7383a0920fb9e7c2b26f2cd8f05
eithan <eithan.nakache@epita.fr>
2023-02-03 02:01:46 +0100

    feat(image): add animation hover icons

--- react_portefolio/pages/index.tsx
+++ react_portefolio/pages/index.tsx
@@ -152,55 +152,55 @@ export default function Home() {
               <ul className="py-5 flex flex-wrap justify-center">
                 <li className="py-3">
                   <img
-                    className="object-scale-down h-24 w-48"
+                    className="object-scale-down h-24 w-48 transform transition-all duration-300 hover:scale-105 hover:shadow-l"
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg"
                   />
                 </li>
                 <li className="py-3">
                   <img
-                    className="object-scale-down h-24 w-48"
+                    className="object-scale-down h-24 w-48 transform transition-all duration-300 hover:scale-105 hover:shadow-l"
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg"
                   />
                 </li>
                 <li className="py-3">
                   <img
-                    className="object-scale-down h-24 w-48"
+                    className="object-scale-down h-24 w-48 transform transition-all duration-300 hover:scale-105 hover:shadow-l"
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg"
                   />
                 </li>
                 <li className="py-3">
                   <img
-                    className="object-scale-down h-24 w-48"
+                    className="object-scale-down h-24 w-48 transform transition-all duration-300 hover:scale-105 hover:shadow-l"
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
                   />
                 </li>
                 <li className="py-3">
                   <img
-                    className="object-scale-down h-24 w-48"
+                    className="object-scale-down h-24 w-48 transform transition-all duration-300 hover:scale-105 hover:shadow-l"
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
                   />
                 </li>
                 <li className="py-3">
                   <img
-                    className="object-scale-down h-24 w-48"
+                    className="object-scale-down h-24 w-48 transform transition-all duration-300 hover:scale-105 hover:shadow-l"
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
                   />
                 </li>
                 <li className="py-5">
                   <img
-                    className="object-scale-down h-24 w-48"
+                    className="object-scale-down h-24 w-48 transform transition-all duration-300 hover:scale-105 hover:shadow-l"
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
                   />
                 </li>
                 <li className="py-5">
                   <img
-                    className="object-scale-down h-24 w-48"
+                    className="object-scale-down h-24 w-48 transform transition-all duration-300 hover:scale-105 hover:shadow-l"
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg"
                   />
                 </li>
                 <li className="py-5">
                   <img
-                    className="object-scale-down h-24 w-48"
+                    className="object-scale-down h-24 w-48 transform transition-all duration-300 hover:scale-105 hover:shadow-l"
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ocaml/ocaml-original.svg"
                   />
                 </li>
@@ -211,55 +211,55 @@ export default function Home() {
               <ul className=" py-5 flex flex-wrap justify-center">
                 <li className="py-2">
                   <img
-                    className="object-scale-down h-24 w-48"
+                    className="object-scale-down h-24 w-48 transform transition-all duration-300 hover:scale-105 hover:shadow-l"
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
                   />
                 </li>
                 <li className="py-5">
                   <img
-                    className="object-scale-down h-24 w-48"
+                    className="object-scale-down h-24 w-48 transform transition-all duration-300 hover:scale-105 hover:shadow-l"
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg"
                   />
                 </li>
                 <li className="py-5">
                   <img
-                    className="object-scale-down h-24 w-48"
+                    className="object-scale-down h-24 w-48 transform transition-all duration-300 hover:scale-105 hover:shadow-l"
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"
                   />
                 </li>
                 <li className="py-5">
                   <img
-                    className="object-scale-down h-24 w-48"
+                    className="object-scale-down h-24 w-48 transform transition-all duration-300 hover:scale-105 hover:shadow-l"
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg"
                   />
                 </li>
                 <li className="py-5">
                   <img
-                    className="object-scale-down h-24 w-48"
+                    className="object-scale-down h-24 w-48 transform transition-all duration-300 hover:scale-105 hover:shadow-l"
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg"
                   />
                 </li>
                 <li className="py-5">
                   <img
-                    className="object-scale-down h-24 w-48"
+                    className="object-scale-down h-24 w-48 transform transition-all duration-300 hover:scale-105 hover:shadow-l"
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain.svg"
                   />
                 </li>
                 <li className="py-5">
                   <img
-                    className="object-scale-down h-24 w-48"
+                    className="object-scale-down h-24 w-48 transform transition-all duration-300 hover:scale-105 hover:shadow-l"
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg"
                   />
                 </li>
                 <li className="py-5">
                   <img
-                    className="object-scale-down h-24 w-48"
+                    className="object-scale-down h-24 w-48 transform transition-all duration-300 hover:scale-105 hover:shadow-l"
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/apple/apple-original.svg"
                   />
                 </li>
                 <li className="py-5">
                   <img
-                    className="object-scale-down h-24 w-48"
+                    className="object-scale-down h-24 w-48 transform transition-all duration-300 hover:scale-105 hover:shadow-l"
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/windows8/windows8-original.svg"
                   />
                 </li>
