d4a3ccbe26d1984439b97e9eb4bb2544ad19e7d9
eithan <eithan.nakache@epita.fr>
2023-02-03 01:51:06 +0100

    feat(skills): add skills

--- react_portefolio/pages/index.tsx
+++ react_portefolio/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import { BsFillMoonStarsFill } from "react-icons/bs";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import Image from "next/image";
 import eithan from "../public/eithan.png";
@@ -33,6 +32,9 @@ export default function Home() {
               </a>
               <a className="text-lg px-4 hover:text-blue-400" href="#Projects">
                 Projects
+              </a>
+              <a className="text-lg px-4 hover:text-blue-400" href="#Skills">
+                Skills
               </a>
               <a className="text-lg px-4 hover:text-blue-400" href="#Contact">
                 Contact
@@ -139,6 +141,138 @@ export default function Home() {
             </div>
           </div>
         </section>
+
+        <section className="py-10">
+        <h2 id="Skills" className="text-5xl text-center text-blue-400 font-medium font-montserrat">
+            Skills
+        </h2>
+
+          <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-10 mt-10">
+            <div className="bg-white rounded-md p-5 shadow-md dark:bg-gray-800">
+              <h3 className="text-xl text-blue-400 font-medium font-montserrat border-b-4">Languages</h3>
+              <ul className="py-5 flex flex-wrap justify-center">
+                <li className="py-3">
+                  <img
+                    className="object-scale-down h-24 w-48"
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg"
+                  />
+                </li>
+                <li className="py-3">
+                  <img
+                    className="object-scale-down h-24 w-48"
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg"
+                  />
+                </li>
+                <li className="py-3">
+                  <img
+                    className="object-scale-down h-24 w-48"
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg"
+                  />
+                </li>
+                <li className="py-3">
+                  <img
+                    className="object-scale-down h-24 w-48"
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
+                  />
+                </li>
+                <li className="py-3">
+                  <img
+                    className="object-scale-down h-24 w-48"
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
+                  />
+                </li>
+                <li className="py-3">
+                  <img
+                    className="object-scale-down h-24 w-48"
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+                  />
+                </li>
+                <li className="py-5">
+                  <img
+                    className="object-scale-down h-24 w-48"
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+                  />
+                </li>
+                <li className="py-5">
+                  <img
+                    className="object-scale-down h-24 w-48"
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg"
+                  />
+                </li>
+                <li className="py-5">
+                  <img
+                    className="object-scale-down h-24 w-48"
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ocaml/ocaml-original.svg"
+                  />
+                </li>
+              </ul>
+            </div>
+            <div className='bg-white rounded-md p-5 shadow-md dark:bg-gray-800'>
+              <h3 className='text-2xl text-teal-600 font-medium'>Framework</h3>
+            <div className="bg-white rounded-md p-5 shadow-md dark:bg-gray-800">
+              <h3 className="text-2xl text-blue-400 font-medium font-montserrat border-b-4">Tools</h3>
+              <ul className=" py-5 flex flex-wrap justify-center">
+                <li className="py-2">
+                  <img
+                    className="object-scale-down h-24 w-48"
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
+                  />
+                </li>
+                <li className="py-5">
+                  <img
+                    className="object-scale-down h-24 w-48"
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg"
+                  />
+                </li>
+                <li className="py-5">
+                  <img
+                    className="object-scale-down h-24 w-48"
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"
+                  />
+                </li>
+                <li className="py-5">
+                  <img
+                    className="object-scale-down h-24 w-48"
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg"
+                  />
+                </li>
+                <li className="py-5">
+                  <img
+                    className="object-scale-down h-24 w-48"
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg"
+                  />
+                </li>
+                <li className="py-5">
+                  <img
+                    className="object-scale-down h-24 w-48"
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain.svg"
+                  />
+                </li>
+                <li className="py-5">
+                  <img
+                    className="object-scale-down h-24 w-48"
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg"
+                  />
+                </li>
+                <li className="py-5">
+                  <img
+                    className="object-scale-down h-24 w-48"
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/apple/apple-original.svg"
+                  />
+                </li>
+                <li className="py-5">
+                  <img
+                    className="object-scale-down h-24 w-48"
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/windows8/windows8-original.svg"
+                  />
+                </li>
+              </ul>
+            </div>
+          </div>
+          </div>
+        </section>
+
+
 
         <section id="Contact" className="py-20">
           <h2 className="text-5xl text-center text-blue-400 font-medium font-montserrat">
