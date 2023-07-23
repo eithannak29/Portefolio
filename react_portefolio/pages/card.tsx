5fc7815d36511d36373ab66469930d328b63b0fa
eithan <eithan.nakache@epita.fr>
2023-07-23 19:02:03 +0200

    fix(card): fix params

--- react_portefolio/pages/card.tsx
+++ react_portefolio/pages/card.tsx
@@ -1,32 +1,39 @@
 import React from 'react';
 
-export default function CreateCard({ name, description, link, icons }) {
-    return (
-      <div className="bg-white bg-opacity-50 rounded-md p-5 shadow-md transform transition-all duration-300 hover:scale-105 hover:shadow-xl">
-        <div className="flex items-center">
-          <h3 className="text-2xl text-blue-400 font-medium font-montserrat">
-            {name}
-          </h3>
-          {icons && icons.length > 0 && (
-            <div className="flex">
-              {icons.map((icon, index) => (
-                <img
-                  key={index}
-                  className="object-scale-down h-8 w-16"
-                  src={icon}
-                />
-              ))}
-            </div>
-          )}
-        </div>
-        <p className="py-5 text-gray-500">{description}</p>
-        <a
-          href={link}
-          className="bg-gray-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded-full"
-        >
-          View
-        </a>
+interface CardProps {
+  name: string;
+  description: string;
+  link: string;
+  icons: string[];
+}
+
+export default function CreateCard({ name, description, link, icons }: CardProps) {
+  return (
+    <div className="bg-white bg-opacity-50 rounded-md p-5 shadow-md transform transition-all duration-300 hover:scale-105 hover:shadow-xl">
+      <div className="flex items-center">
+        <h3 className="text-2xl text-blue-400 font-medium font-montserrat">
+          {name}
+        </h3>
+        {icons && icons.length > 0 && (
+          <div className="flex">
+            {icons.map((icon, index) => (
+              <img
+                key={index}
+                className="object-scale-down h-8 w-16"
+                src={icon}
+                alt={`Icon ${index + 1}`}
+              />
+            ))}
+          </div>
+        )}
       </div>
-    );
-  }
-  
+      <p className="py-5 text-gray-500">{description}</p>
+      <a
+        href={link}
+        className="bg-gray-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded-full"
+      >
+        View
+      </a>
+    </div>
+  );
+}
