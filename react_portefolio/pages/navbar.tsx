7c95e276fd07a3aef7992d02ee9ea9c565a26495
eithan <eithan.nakache@epita.fr>
2023-07-31 03:38:49 +0200

    fix(navbar): navbar fixed

--- react_portefolio/pages/navbar.tsx
+++ react_portefolio/pages/navbar.tsx
@@ -8,6 +8,8 @@ import SplitType from "split-type";
 
 import { gsap } from "gsap";
 
+import { Navbar } from "flowbite-react";
+
 export default function NavBar() {
   useEffect(() => {
     let text = new SplitType("#name");
@@ -24,78 +26,33 @@ export default function NavBar() {
   }, []);
 
   return (
-    <div className="">
-      <section id="About_Me" className="min-h-screen">
-        
-        <nav className="top-0 right-0">
-          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <div />
-            <button
-              data-collapse-toggle="navbar-default"
-              type="button"
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="navbar-default"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 17 14"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 1h15M1 7h15M1 13h15"
-                />
-              </svg>
-            </button>
-            <div
-              className="hidden w-full md:block md:w-auto"
-              id="navbar-default"
-            >
-              <ul className="font-medium flex flex-col p-4 md:flex-row md:p-0 md:items-center md:justify-center text-center md:text-left md:space-x-8 bg-gray-800 bg-opacity-50 rounded-md ">
-                <li>
-                  <a
-                    href="#Home"
-                    className="text-lg text-white px-4 hover:text-blue-400"
-                    aria-current="page"
-                  >
-                    Home
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#Projects"
-                    className="text-lg text-white px-4 hover:text-blue-400"
-                  >
-                    Projects
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#Skills"
-                    className="text-lg text-white px-4 hover:text-blue-400"
-                  >
-                    Skills
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#Contact"
-                    className="text-lg px-4 text-white hover:text-blue-400"
-                  >
-                    Contact
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+    <div className="py-4">
+      <section id="About_Me">
+        <div className="flex justify-end">
+          <Navbar
+            fluid
+            rounded
+            className="bg-gray-800 bg-opacity-50 rounded max-w-min"
+          >
+            <Navbar.Brand />
+            <Navbar.Toggle/>
+
+            <Navbar.Collapse>
+              <Navbar.Link href="#Home" className="text-white ">
+              <div className="hover:text-blue-400"> Home </div>
+              </Navbar.Link>
+              <Navbar.Link href="#Projects" className="text-white ">
+                <div className="hover:text-blue-400"> Projects </div>
+              </Navbar.Link>
+              <Navbar.Link href="#Skills" className="text-white">
+              <div className="hover:text-blue-400"> Skills </div>
+              </Navbar.Link>
+              <Navbar.Link href="#Contact" className="text-white">
+              <div className="hover:text-blue-400"> Contact </div>
+              </Navbar.Link>
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
 
         <div id="Home" className="text-center p-10">
           <h1
